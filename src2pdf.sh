fbf2fff643b28578cbc3a664666b92a29b6fc29a
vegito2002 <vegito2002@gmail.com>
2017-03-05 14:04:15 -0500

    now the src2pdf bash can handle file names with _ character in it

--- src2pdf.sh
+++ src2pdf.sh
@@ -11,7 +11,7 @@ cat<<EOF >$tex_file   ## Print the tex file header
 \lstdefinestyle{customasm}{
   belowcaptionskip=1\baselineskip,
   xleftmargin=\parindent,
-  language=java,   %% Change this to whatever you write in
+  language=Java,   %% Change this to whatever you write in
   breaklines=true, %% Wrap long lines
   basicstyle=\ttfamily,
   numbers=left,
@@ -33,7 +33,7 @@ sed 's/^\..//' |                 ## Change ./foo/bar.src to foo/bar.src
 while read  i; do                ## Loop through each file
 
    echo "\newpage" >> $tex_file   ## start each section on a new page
-    echo "\section{$i}" >> $tex_file  ## Create a section for each file
+    echo "\section{$i }" | tr _ . >> $tex_file  ## Create a section for each file
 
    ## This command will include the file in the PDF
     echo "\lstinputlisting[style=customasm]{$i}" >>$tex_file
